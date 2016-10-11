ee05055122e95e2b202f0e9e5257cb44eaa97536
Brad <brad@brad.tw>
2016-10-11 18:56:22 +0800

    v2

--- app/src/main/java/brad/tw/mywebviewtest/MainActivity.java
+++ app/src/main/java/brad/tw/mywebviewtest/MainActivity.java
@@ -2,12 +2,23 @@ package brad.tw.mywebviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview = (WebView)findViewById(R.id.webview);
+        initWebView();
+
     }
+
+    private void initWebView(){
+        webview.loadUrl("http://www.pchome.com.tw");
+    }
+
 }
